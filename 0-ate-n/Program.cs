fb63965c23dd45128659ccccf1ace7a6b43151ec
Erissim <146840753+ErissonGussoni@users.noreply.github.com>
2024-12-17 16:36:18 -0300

    Add files via upload

---
+++ 0-ate-n/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ate_n
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            string str = "O número inserido foi: " + numero + ".";
+            Console.WriteLine(str);
+            for (int i = 0; i <= numero; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
